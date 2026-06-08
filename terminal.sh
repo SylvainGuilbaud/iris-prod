ea08bd41983ce8ce9222902f72620eed94cf5707
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2026-06-08 17:37:54 +0200

    init

---
+++ terminal.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+TARGET=${1:-iris}
+
+case "$TARGET" in
+    iris)
+        CONTAINER="iris-test"
+        ;;
+    prod1)
+        CONTAINER="iris-prod-1"
+        ;;
+    prod2)
+        CONTAINER="iris-prod-2"
+        ;;
+    *)
+        echo "Usage: $0 [iris|prod1|prod2]"
+        exit 1
+        ;;
+esac
+
+docker exec -it "$CONTAINER" iris session iris -U IRISAPP
